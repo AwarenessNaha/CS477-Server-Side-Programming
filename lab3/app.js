d2245bb5a0bde9d52e87abd5a651423c97a0d851
Naha Shiva <franklinbeing@gmail.com>
2021-06-27 22:53:26 -0500

    lab3 solution

---
+++ lab3/app.js
@@ -0,0 +1,51 @@
+/**
+ * 
+1, Create a http or https server which is listen to 3000 port.
+2. The home page “/” which displays an html page with one input to enter any text message
+3. User enter some message, then click “Submit” button
+4. The user’s inputs are stored in a local file on the server side.
+5. User will be redirect to home page after saving successfully.
+ */
+
+//const http = require('http');
+//const fs = require('fs');
+
+const server = require('https').createServer();
+
+server.on('request', (req, res) => {
+    // console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/messsage" method="POST">Enter Message: <input name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+     res.end(); // "retrun" here exits the function execution, otherwise continue.
+    }
+    else if (url === '/messsage' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            fs.writeFileSync('message.txt', parsedBody.split('=')[1]);
+        });
+
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+    } else{
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hellow from Node.js</h1></body>');
+    res.write('</html>');
+    res.end();
+    }
+});
+
+server.listen(3344);
